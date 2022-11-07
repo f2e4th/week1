47a96a49c53b6b5350831b0326f4a153233f3732
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2022-11-07 15:01:41 +0800

    feat:加gsap套件 / 引入gsap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { gsap } from 'gsap';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(gsap)
+   .mount('#app');
