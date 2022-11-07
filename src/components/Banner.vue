a8199d7fa73b69f472f77738b3ad42e006d5d2bf
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2022-11-07 17:35:26 +0800

    修正 cx

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,10 +3,10 @@
     <h1 class="banner-title">The F2E 4TH </h1>
     <div class="banner-svg banner-svg--back">
         <svg class="w-full h-full">
-            <circle cx="50" cy="50" r="50" class="svg-circle svg-circle--1"></circle>
-            <circle cx="50" cy="50" r="60" class="svg-circle svg-circle--2"></circle>
-            <circle cx="50" cy="50" r="70" class="svg-circle svg-circle--3"></circle>
-            <circle cx="50" cy="50" r="80" class="svg-circle svg-circle--4"></circle>
+            <circle cx="439" cy="100" r="80" class="svg-circle svg-circle--1"></circle>
+            <circle cx="439" cy="100" r="70" class="svg-circle svg-circle--2"></circle>
+            <circle cx="439" cy="100" r="60" class="svg-circle svg-circle--3"></circle>
+            <circle cx="439" cy="100" r="50" class="svg-circle svg-circle--4"></circle>
         </svg>
     </div>
     <div class="banner-svg banner-svg--front"></div>
@@ -20,7 +20,9 @@ export default {
     setup(){
         onMounted( ()=>{
             gsapInit();
+            getCircyleY();
         })
+        const circyleY =  ref(0);
         function gsapInit(){
             const timeline = gsap.timeline();
             console.log(timeline);
@@ -40,7 +42,14 @@ export default {
             return backHeight-frontHeight-titleHeight - 20;
         }
         function getCircyleY(){
-            return (window.innerWidth - 16)/2
+            // return (window.innerWidth - 16)/2
+            var circyle1 = document.querySelector('.svg-circle--1').attributes[2]
+            var circyle2 = document.querySelector('.svg-circle--2').attributes[2]
+            var circyle3 = document.querySelector('.svg-circle--3').attributes[2]
+            var circyle4 = document.querySelector('.svg-circle--4').attributes[2]
+
+            console.log(typeof circyle1, circyle2, circyle3, circyle4)
+            // circyleY.value = (window.innerWidth - 32 - )/2;
         }
     }
 
