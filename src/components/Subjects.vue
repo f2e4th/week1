a031af21a75c1b158b95934f6e98124fb8624dea
Shang <yopshang@gmail.com>
2022-11-10 22:30:19 +0800

    優化

--- src/components/Subjects.vue
+++ src/components/Subjects.vue
@@ -1,121 +1,114 @@
 <template>
   <div id="Subjects">
     <div class="title">
-        <h1>WEEK 1</h1>
-        <h2>The F2E 活動網站設計</h2>
+      <h1>WEEK 1</h1>
+      <h2>The F2E 活動網站設計</h2>
     </div>
     <div class="img_layout">
-        <div class="img_container">
-            <div class="img_div img_div--3" >
-                <img src="../assets/img/img_week3_big.png" alt="img">
-            </div>
-            <div class="img_div img_div--2">
-                <img src="../assets/img/img_week2_big.png" alt="img">
-            </div>
-            <div class="img_div img_div--1">
-                <img src="../assets/img/img_week1_big.png" alt="img">
-            </div>
+      <div class="img_container">
+        <div class="img_div img_div--3">
+          <img src="../assets/img/img_week3_big.png" alt="img" />
         </div>
+        <div class="img_div img_div--2">
+          <img src="../assets/img/img_week2_big.png" alt="img" />
+        </div>
+        <div class="img_div img_div--1">
+          <img src="../assets/img/img_week1_big.png" alt="img" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
 <script>
-import {ref, reactive, onMounted, computed, watch} from "vue";
-import gsap from "gsap"
-import scrollTrigger from "gsap/scrollTrigger"
-export default {
-    setup(){
-        onMounted( ()=>{
-            gsapInit();
-        })
-        const imgs = reactive([
-            "../assets/img/img_week3_big.png",
-            "../assets/img/img_week2_big.png",
-            "../assets/img/img_week1_big.png"
-        ])
-        function gsapInit(){
-            gsap.registerPlugin(scrollTrigger);
-            const timeline = gsap.timeline();
-            timeline.to('#Subjects', {
-                scrollTrigger:{
-                    trigger: '#Subjects',
-                    pin: true, // 要postion fixed 的時候 pin需要設為true,
-                    markers: false,
-                    scrub: true
-                }
-            })
-            timeline.to('.img_div--1', {
-                scrollTrigger:{
-                    trigger: '#Subjects',
-                    markers: false,
-                    start: 'top',
-                    end: 'center 30%',
-                    height: 0,
-                    scrub: true
-                },
-                height: 0,
-            })
-            timeline.to('.img_div--2', {
-                scrollTrigger:{
-                    trigger: '#Subjects',
-                    markers: true,
-                    start: 'center 30%',
-                    end: '',
-                    height: 0,
-                    scrub: true
-                },
-                height: 0,
-            })
-            // timeline.to('.img_div--3', {
-            //     scrollTrigger:{
-            //         trigger: '#Subjects',
-            //         markers: false,
-            //         start: 'top 66%',
-            //         end: 'bottom',
-            //         height: 0,
-            //         scrub: true
-            //     },
-            //     height: 0,
-            // })
+import { ref, reactive, onMounted, computed, watch } from "vue";
+import gsap from "gsap";
+import scrollTrigger from "gsap/scrollTrigger";
+</script>
 
-        }
-    }
-
+<script setup>
+onMounted(() => {
+  gsapInit();
+});
+const imgs = reactive([
+  "../assets/img/img_week3_big.png",
+  "../assets/img/img_week2_big.png",
+  "../assets/img/img_week1_big.png",
+]);
+function gsapInit() {
+  gsap.registerPlugin(scrollTrigger);
+  const timeline = gsap.timeline();
+  timeline.to("#Subjects", {
+    scrollTrigger: {
+      trigger: "#Subjects",
+      pin: true, // 要postion fixed 的時候 pin需要設為true,
+      markers: false,
+      scrub: true,
+    },
+  });
+  timeline.to(".img_div--1", {
+    scrollTrigger: {
+      trigger: "#Subjects",
+      markers: false,
+      start: "top",
+      end: "center 30%",
+      height: 0,
+      scrub: true,
+    },
+    height: 0,
+  });
+  timeline.to(".img_div--2", {
+    scrollTrigger: {
+      trigger: "#Subjects",
+      markers: true,
+      start: "center 30%",
+      end: "",
+      height: 0,
+      scrub: true,
+    },
+    height: 0,
+  });
+  // timeline.to('.img_div--3', {
+  //     scrollTrigger:{
+  //         trigger: '#Subjects',
+  //         markers: false,
+  //         start: 'top 66%',
+  //         end: 'bottom',
+  //         height: 0,
+  //         scrub: true
+  //     },
+  //     height: 0,
+  // })
 }
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/scss/all.scss';
-#Subjects{
-    height: 1024px;
-    background: $main_bg;
+@import "../assets/scss/all.scss";
+#Subjects {
+  height: 1024px;
+  background: $main_bg;
 }
 .title {
-    font-weight: 500;
-    font-size: 40px;
-    line-height: 60px;
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 60px;
 }
-.img_layout{
-    @apply flex justify-center;
+.img_layout {
+  @apply flex justify-center;
 }
 .img_container {
-    @apply relative w-full;
-        width: 600px;
-        height: 600px;
-    img {
-        
-        width: 600px;
-        height: 600px;
-    }
+  @apply relative w-full;
+  width: 600px;
+  height: 600px;
+  img {
+    width: 600px;
+    height: 600px;
+  }
 }
 .img_div {
-    @apply absolute top-0;
-    height: 600px;
-    width: 600px;
-    overflow: hidden;
-    background: $main_bg;
-
+  @apply absolute top-0;
+  height: 600px;
+  width: 600px;
+  overflow: hidden;
+  background: $main_bg;
 }
-
 </style>
