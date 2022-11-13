4dda2bd42d4586916ded71c21ba9877a27394c39
Eva <shepherdlord1119@gmail.com>
2022-11-13 20:20:38 +0800

    修正main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { gsap } from 'gsap';
 
 const app = createApp(App);
 
-app.use(gsap)
-   .mount('#app');
+app.mount('#app');
