fb664af17fb665ed748e9cc05b7b532950f16535
NEWEBOA\frank.hsu <frank_hsu@neweb.com.tw>
2022-11-14 15:33:13 +0800

    try gsap

--- src/components/askQuestion.vue
+++ src/components/askQuestion.vue
@@ -1,36 +1,36 @@
 <template>
     <div class='wrapper' id='askQuestion'>
         
-         <!-- 1 -->
-        <div class="unhappyQuestion Question_img">
-            <img src="../assets/images/unhappyQuestion.png" alt="">
+        <!-- 3 -->
+        <div class="sadQuestion Question_img" style='opacity:0'>
+            <img src="../assets/images/sadQuestion.png" alt="">
         </div>
-        <div class='unhappy magic_img'>
-            <img src="../assets/images/unhappy.png" alt="">
+        <div class='sad magic_img mc_3'>
+            <img src="../assets/images/sad.png" alt="">
         </div>
-        
-         <!-- 2 -->
-        <!-- <div class="ohQuestion Question_img">
+
+        <!-- 2 -->
+        <div class="ohQuestion Question_img" style='opacity:0'>
             <img src="../assets/images/ohQuestion.png" alt="">
-        </div> -->
-        <div class='oh magic_img' style='opacity:0'>
+        </div>
+        <div class='oh magic_img mc_2'>
             <img src="../assets/images/oh.png" alt="">
         </div>
 
-        <!-- 3 -->
-        <!-- <div class="sadQuestion Question_img">
-            <img src="../assets/images/sadQuestion.png" alt="">
-        </div> -->
-        <!-- <div class='sad magic_img'>
-            <img src="../assets/images/sad.png" alt="">
-        </div> -->
-
+        <!-- 1 -->
+        <div class="unhappyQuestion Question_img">
+            <img src="../assets/images/unhappyQuestion.png" alt="">
+        </div>
+        <div class='unhappy magic_img mc_1'>
+            <img src="../assets/images/unhappy.png" alt="">
+        </div>
+    
     </div>
 </template>
 
 <script>
 import gsap from 'gsap';
-import scrollTrigger from 'gsap/scrollTrigger'
+import scrollTrigger from 'gsap/scrollTrigger';
 import { onMounted } from '@vue/runtime-core';
 
 export default {
@@ -44,62 +44,86 @@ export default {
 function gsapInit(){
     gsap.registerPlugin(scrollTrigger);
     const timeline = gsap.timeline();
-    // timeline.to('#askQuestion',{
-    //     scrollTrigger:{
-    //         trigger:"#askQuestion",
-    //         pin:true,
-    //         pinSpacing:false,
-    //         markers:false,
-    //         scrub:true,
-    //         start:'top',
-    //         end:'bottom'
-    //     },
-    // });
-
-    // timeline.to(".oh", {
-    //     scrollTrigger: {
-    //         trigger: ".oh",
-    //         markers: true,
-    //         start: "top center",
-    //         end: "1%",
-    //         height: 0,
-    //         scrub: true,
-    //     },
-    // height: 0,
-    // });
-    // timeline.to(".oh", {
-    //     scrollTrigger: {
-    //         trigger: ".oh",
-    //         markers: true,
-    //         start: "bottom top",
-    //         end: "5%",
-    //         height: 0,
-    //         scrub: true,
-    //     },
-    // height: 0,
-    // });
-
-    gsap.to('.unhappy',{
-        scrollTrigger:{
-            trigger:'#askQuestionContent',
-            pin:true,
-            strat:'center center',
-            end:'bottom',
-            markers:true,
-            scrub: true
+    const 
+    _1to2 = {
+        scrollTrigger: {
+            trigger: ".unhappyQuestion",
+            start: "55% 30%",
+            end: "bottom",
+            scrub: false,
         },
         opacity:0,
-        duration:3,
-    })
-    // timeline.to('.oh',{
-    //     scrollTrigger:{
-    //         trigger:'.oh',
-    //         strat:'center',
-    //         markers:false,
-    //     },
-    //     opacity:1,
-    //     duration:3
-    // })
+        duration:0.3,
+    },
+
+    _1to2_setOP = {
+        scrollTrigger: {
+            trigger: ".ohQuestion",
+            start: "55% 30%",
+            end: "bottom",
+            scrub: false,
+        },
+        opacity:1,
+        duration:0.3,
+    },
+
+    _2to3 = {
+        scrollTrigger: {
+            trigger: ".ohQuestion",
+            start: "bottom top",
+            end: "bottom",
+            scrub: false,
+        },
+        opacity:0,
+        duration:0.3,
+    },
+
+    _2to3_setOP = {
+        scrollTrigger: {
+            trigger: ".sadQuestion",
+            start: "bottom -10%",
+            end: "bottom",
+            scrub: false,
+        },
+        opacity:1,
+        duration:0.3,
+    }
+
+
+    timeline.to(".mc_1", {
+        scrollTrigger: {
+            pin:true,
+            trigger: "#askQuestion",
+            // markers: {
+            //     startColor: 'pink',
+            //     endColor: 'pink'
+            // },
+            start: "45% 40%",
+            end: "bottom",
+            scrub: false,
+        },
+        opacity:0,
+        duration:0.3,
+    });
+
+
+    timeline.to(".mc_2", {
+        scrollTrigger: {
+            pin:true,
+            trigger: "#askQuestion",
+            markers:false,
+            start: "center 40%",
+            end: "bottom",
+            scrub: false,
+        },
+        opacity:0,
+        duration:0.3,
+    });
+
+    timeline.to(".unhappyQuestion", _1to2);
+    timeline.to(".ohQuestion", _1to2_setOP);
+    timeline.to(".ohQuestion", _2to3,);
+    timeline.to(".sadQuestion", _2to3_setOP);
 }
 
 </script>
@@ -109,7 +133,7 @@ function gsapInit(){
     .wrapper{
         // background-color: rgba(238, 240, 241, 1);
         position: relative;
-        // width:1440px;
+        width:100%;
         height: 1024px;
         margin: 0 auto;
         background-image: url('../assets/images/Bg_big.png');
@@ -132,7 +156,7 @@ function gsapInit(){
         overflow: hidden;
 
         img{
-            width: 620px;
+            width: 100%;
         }
     }
 
