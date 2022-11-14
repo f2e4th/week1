609b6203a9981791055b6c9ffcb5c08b3565e8b7
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2022-11-14 16:17:11 +0800

    改config設定

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./week1/dist/',
+  base:'/week1/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+         '/images': 'src/assets/images',
+    },
+},
 })
